899060904649da9c59d762e96faeaf2b04e67fa4
mtrimner <matt.trimner@gmail.com>
2020-09-30 10:30:09 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class App < Sinatra::Base
         @team_members.each do |hero|
             @hero_name << hero[:name]
             @hero_power << hero[:power]
-            @hero_bio << hero[:bio]
+            @hero_bio << hero[:biography]
         end
         erb :team
     end
